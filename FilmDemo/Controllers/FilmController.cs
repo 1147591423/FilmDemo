e4c5d621d35060e2ce54b3d9bcf0163d7a342c60
1147591423 <51426658+1147591423@users.noreply.github.com>
2019-06-10 08:49:41 +0800

    添加项目文件。

---
+++ FilmDemo/Controllers/FilmController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmDemo.Controllers
+{
+    public class FilmController : Controller
+    {
+        // GET: Film
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FistPage()
+        {
+            return View();
+        }
+        public ActionResult FilmPage()
+        {
+            return View();
+        }
+        public ActionResult CinemaPage()
+        {
+            return View();
+        }
+        public ActionResult ListPage()
+        {
+            return View();
+        }
+        public ActionResult FilmInfo()
+        {
+            return View();
+        }
+        public ActionResult GetTicket()
+        {
+            return View();
+        }
+        public ActionResult GetSitAndGetTicket()
+        {
+            return View();
+        }
+        public ActionResult GetSit()
+        {
+            return View();
+        }
+        public ActionResult CfmInfo()
+        {
+            return View();
+        }
+        public ActionResult ChoosePayWay()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ FilmDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FilmDemo.MvcApplication" Language="C#" %>
